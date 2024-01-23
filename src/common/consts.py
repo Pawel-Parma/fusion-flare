1929bf01c808e7d8d18ae8ff9ac71eb57b8bf05f
Pawel-Parma <122152230+Pawel-Parma@users.noreply.github.com>
2024-01-23 17:50:25 +0100

    feat: simple window

--- src/common/consts.py
+++ src/common/consts.py
@@ -1,1 +1,6 @@
+import logging
 
+LOGGER: logging.Logger = logging.getLogger("main")
+
+WIDTH: int = 1300
+HEIGHT: int = 850
--- src/main.py
+++ src/main.py
@@ -1,5 +1,38 @@
+import sys
+# =================================
+import glfw
+# =================================
+from OpenGL.GL import *
+# =================================
+from src.common import *
+
+if not glfw.init():
+    LOGGER.error("Failed to initialize glfw")
+    sys.exit(-1)
+
+
+class App:
+    def __init__(self) -> None:
+        self.window = glfw.create_window(WIDTH, HEIGHT, "Labiryntho", None, None)
+        if not self.window:
+            glfw.terminate()
+            LOGGER.error("Failed to create window")
+            sys.exit(-2)
+
+        glfw.set_window_pos(self.window, 100, 100)
+        glfw.make_context_current(self.window)
+
+    def mainloop(self) -> None:
+        while not glfw.window_should_close(self.window):
+            glfw.poll_events()
+            glfw.swap_buffers(self.window)
+
+        glfw.terminate()
+
+
 def main() -> None:
-    print("Hello, world!")
+    app = App()
+    app.mainloop()
 
 
 if __name__ == "__main__":
