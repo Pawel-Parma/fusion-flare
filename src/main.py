18dbf94ff50b73750c09bed94b3317909dbb3f13
Pawel-Parma <122152230+Pawel-Parma@users.noreply.github.com>
2024-01-27 22:52:03 +0100

    refactor: moved TODOs to main.py

--- src/main.py
+++ src/main.py
@@ -10,6 +10,49 @@ from scene_renderer import SceneRenderer
 from maze import *
 from common import *
 
+# (GAME)
+# (TODO: Make faster by utilising chunks and not rendering everything at once)
+# (TODO: Add icon)
+# (TODO: ask before quiting game)
+# (TODO: add f3 debug screen)
+# (TODO: show time, coins and score when playing)
+
+# (TODO: Make main menu and its functionality)
+# (Play button and screen)
+# (Settings button and screen [audio, graphics, controls, credits])
+# (Exit button)
+# (Shop button [coming soon])
+# (History button)
+
+# (TODO: Make escape menu)
+# (Resume)
+# (Save)
+# (Settings)
+# (Exit)
+
+# (TODO: Make maze generation)
+
+# (TODO: Make end screen)
+# (Time)
+# (Score)
+# (Coins)
+# (Name)
+# (Play-through [shows correct path with green])
+
+# (Play again [shows play screen and same setting])
+# (Main menu)
+
+# (TODO: Make database)
+
+# (IF TIME)
+# (TODO: Make coin power-ups [eg. has compass, shows where to go, speed boost, etc.])
+# (TODO: Add shop)
+# (TODO: Custom textures [for player to load])
+# (TODO: Custom music)
+# (TODO: Allow 3D with stairs)
+# (TODO: enemies)
+# (TODO: Make custom map generator [might be hard])
+
 
 class GraphicsEngine:  # TODO: clean up the project structure
     def __init__(self) -> None:
