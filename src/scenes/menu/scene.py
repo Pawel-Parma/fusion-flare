984ac0976e69dda821179547571d526fc847080b
Pawel-Parma <122152230+Pawel-Parma@users.noreply.github.com>
2024-02-05 19:16:44 +0100

    refactor: Made better menu scene for button testing

--- src/scenes/menu/scene.py
+++ src/scenes/menu/scene.py
@@ -16,34 +16,58 @@ class MenuScene:
     def load(self):
         app = self.app
         add = self.add_object
+        buttons = []
+        colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255), (255, 0, 255), (255, 255, 0), (255, 165, 0),
+                  (128, 0, 128), (255, 105, 180)]
 
-        b1 = add(Button(app, position=(0, 2, 0), hover_color=(125, 125, 125), color=(255, 0, 0)))
-        b2 = add(Button(app, position=(0, -2, 0), hover_color=(125, 125, 125), color=(0, 255, 0)))
-        b1.set_chosen()
-        # b1.down_button(b2)
-        # b2.up_button(b1)
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                color = colors[(i * 3 + j + 4) % len(colors)]
+                button = add(Button(app, position=(4 * i, 4 * j, 0), hover_color=(155, 155, 155),
+                                    color=color))
+                buttons.append(button)
 
-        # b3 = add(Button(app, position=(-1, -7, 2), color=(125, 125, 125), hover_color=(0, 255, 0)))
-        # b4 = add(Button(app, position=(-1, -10, 2), color=(125, 125, 125), hover_color=(0, 255, 0)))
-        # b5 = add(Button(app, position=(-1, -13, 2), color=(125, 125, 125), hover_color=(0, 255, 0)))
+        buttons[0].right_button(buttons[1])
+        buttons[0].down_button(buttons[3])
 
-        add(Cube(app, "white", position=(0, 4, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
-        add(Cube(app, "white", position=(2, 4, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
-        add(Cube(app, "white", position=(-2, 4, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
+        buttons[1].left_button(buttons[0])
+        buttons[1].right_button(buttons[2])
+        buttons[1].down_button(buttons[4])
 
-        add(Cube(app, "white", position=(2, 2, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
-        add(Cube(app, "white", position=(-2, 2, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
+        buttons[2].left_button(buttons[1])
+        buttons[2].down_button(buttons[5])
 
-        add(Cube(app, "white", position=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
-        add(Cube(app, "white", position=(2, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
-        add(Cube(app, "white", position=(-2, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
+        buttons[3].up_button(buttons[0])
+        buttons[3].right_button(buttons[4])
+        buttons[3].down_button(buttons[6])
 
-        add(Cube(app, "white", position=(2, -2, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
-        add(Cube(app, "white", position=(-2, -2, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
+        buttons[4].up_button(buttons[1])
+        buttons[4].left_button(buttons[3])
+        buttons[4].right_button(buttons[5])
+        buttons[4].down_button(buttons[7])
+        buttons[4].set_chosen()
 
-        add(Cube(app, "white", position=(0, -4, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
-        add(Cube(app, "white", position=(2, -4, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
-        add(Cube(app, "white", position=(-2, -4, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
+        buttons[5].up_button(buttons[2])
+        buttons[5].left_button(buttons[4])
+        buttons[5].down_button(buttons[8])
+
+        buttons[6].up_button(buttons[3])
+        buttons[6].right_button(buttons[7])
+
+        buttons[7].up_button(buttons[4])
+        buttons[7].left_button(buttons[6])
+        buttons[7].right_button(buttons[8])
+
+        buttons[8].up_button(buttons[5])
+        buttons[8].left_button(buttons[7])
+
+        for i in range(-2, 2):
+            for j in range(-3, 4):
+                add(Cube(app, "white", position=(2 * j, 2 + 4 * i, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
+
+        for i in range(-2, 2):
+            for j in range(-1, 2):
+                add(Cube(app, "white", position=(2 + 4 * i, 4 * j, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
 
     def update(self):
         pass
