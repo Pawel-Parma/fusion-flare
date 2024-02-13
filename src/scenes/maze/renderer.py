7892f5c30ee171059eeb603a992e9b2c988a5bc3
Pawel-Parma <122152230+Pawel-Parma@users.noreply.github.com>
2024-02-13 19:39:09 +0100

    refactor: abstraction over renderer objects

--- src/scenes/maze/renderer.py
+++ src/scenes/maze/renderer.py
@@ -1,30 +1,11 @@
-from config import *
+from scenes.renderer import BaseRenderer
 
 from .scene import MazeScene
 
 
-class MazeSceneRenderer:
+class MazeSceneRenderer(BaseRenderer):
     def __init__(self, app):
-        self.app = app
-        self.ctx = app.ctx
-        self.mesh = app.mesh
-        self.scene = MazeScene(app)
-        # depth buffer
-        self.depth_texture = self.mesh.texture['depth_texture']
-        self.depth_fbo = self.ctx.framebuffer(depth_attachment=self.depth_texture)
+        super().__init__(app)
 
-    def render_shadow(self):
-        self.depth_fbo.clear()
-        self.depth_fbo.use()
-        for obj in self.scene.shadow_objects:
-            obj.render_shadow()
-
-    def main_render(self):
-        self.app.ctx.screen.use()
-        for obj in (self.scene.no_shadow_objects + self.scene.shadow_objects):
-            obj.render()
-
-    def render(self):
-        self.main_render()
-        if not DISABLE_SHADOW_RENDER:
-            self.render_shadow()
+    def create_scene(self):
+        return MazeScene(self.app)
--- src/scenes/renderer.py
+++ src/scenes/renderer.py
@@ -1,1 +1,35 @@
+import abc
+
 from config import *
+
+
+class BaseRenderer:
+    def __init__(self, app):
+        self.app = app
+        self.ctx = app.ctx
+        self.mesh = app.mesh
+        # scene
+        self.scene = self.create_scene()
+        # depth buffer
+        self.depth_texture = self.mesh.texture['depth_texture']
+        self.depth_fbo = self.ctx.framebuffer(depth_attachment=self.depth_texture)
+
+    def shadow_render(self):
+        self.depth_fbo.clear()
+        self.depth_fbo.use()
+        for obj in self.scene.shadow_objects:
+            obj.render_shadow()
+
+    def main_render(self):
+        self.app.ctx.screen.use()
+        for obj in (self.scene.no_shadow_objects + self.scene.shadow_objects):
+            obj.render()
+
+    def render(self):
+        self.main_render()
+        if not DISABLE_SHADOW_RENDER:
+            self.shadow_render()
+
+    @abc.abstractmethod
+    def create_scene(self):
+        pass
