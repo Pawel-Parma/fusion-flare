6845c05a44be6f3cdc201bba2d1c9ce5f874efab
Pawel-Parma <122152230+Pawel-Parma@users.noreply.github.com>
2024-02-13 19:39:09 +0100

    refactor: abstraction over renderer objects

--- src/scenes/end_game_menu/renderer.py
+++ src/scenes/end_game_menu/renderer.py
@@ -1,30 +1,11 @@
-from config import *
+from scenes.renderer import BaseRenderer
 
 from .scene import EndGameMenu
 
 
-class EndGameMenuSceneRenderer:
+class EndGameMenuSceneRenderer(BaseRenderer):
     def __init__(self, app):
-        self.app = app
-        self.scene = EndGameMenu(app)
-        self.ctx = app.ctx
-        self.mesh = app.mesh
-        # depth buffer
-        self.depth_texture = self.app.mesh.texture['depth_texture']
-        self.depth_fbo = self.ctx.framebuffer(depth_attachment=self.depth_texture)
+        super().__init__(app)
 
-    def shadow_render(self):
-        self.depth_fbo.clear()
-        self.depth_fbo.use()
-        for obj in self.scene.shadow_objects:
-            obj.render_shadow()
-
-    def main_render(self):
-        self.app.ctx.screen.use()
-        for obj in (self.scene.no_shadow_objects + self.scene.shadow_objects):
-            obj.render()
-
-    def render(self):
-        self.main_render()
-        if not DISABLE_SHADOW_RENDER:
-            self.render_shadow()
+    def create_scene(self):
+        return EndGameMenu(self.app)
--- src/scenes/esc_menu/renderer.py
+++ src/scenes/esc_menu/renderer.py
@@ -1,30 +1,11 @@
-from config import *
+from scenes.renderer import BaseRenderer
 
 from .scene import EscMenu
 
 
-class EscMenuSceneRenderer:
+class EscMenuSceneRenderer(BaseRenderer):
     def __init__(self, app):
-        self.app = app
-        self.ctx = app.ctx
-        self.mesh = app.mesh
-        self.scene = EscMenu(app)
-        # depth buffer
-        self.depth_texture = self.mesh.texture['depth_texture']
-        self.depth_fbo = self.ctx.framebuffer(depth_attachment=self.depth_texture)
+        super().__init__(app)
 
-    def shadow_render(self):
-        self.depth_fbo.clear()
-        self.depth_fbo.use()
-        for obj in self.scene.shadow_objects:
-            obj.render_shadow()
-
-    def main_render(self):
-        self.app.ctx.screen.use()
-        for obj in (self.scene.no_shadow_objects + self.scene.shadow_objects):
-            obj.render()
-
-    def render(self):
-        self.main_render()
-        if not DISABLE_SHADOW_RENDER:
-            self.shadow_render()
+    def create_scene(self):
+        return EscMenu(self.app)
