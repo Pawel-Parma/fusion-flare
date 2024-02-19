028c204e3064fb91deb048cc3a396fa66fe17ef1
Pawel-Parma <122152230+Pawel-Parma@users.noreply.github.com>
2024-02-19 21:23:14 +0100

    refactor: the button class is now clearer

--- src/models/button.py
+++ src/models/button.py
@@ -8,79 +8,45 @@ import pygame as pg
 from .base import BaseModel
 
 
-class Button(BaseModel):  # TODO: Refactor this class
-    last_click_time = time.time()
+class Button(BaseModel):
+    last_press_time = time.time()
 
     def __init__(self, app, position, default_texture, hover_texture,
                  change_delay_time=0.15, sequent_press_delay_time=0.3,
                  rotation=(0, 0, 0), scale=(1, 1), is_dynamic=False, alpha=1.0):
         super().__init__(app, "plane2d", "none", position, rotation, (*scale, 1), alpha)
-        self.key_binds = app.key_binds
-        self.func_on_click = lambda: None
-        self.button_up: Button | None = None
-        self.button_up_taken = False
-        self.button_down: Button | None = None
-        self.button_down_taken = False
-        self.button_left: Button | None = None
-        self.button_left_taken = False
-        self.button_right: Button | None = None
-        self.button_right_taken = False
-        self.is_chosen = False
-        self.change_delay_time = change_delay_time
-        self.press_delay_time = sequent_press_delay_time
         self.default_texture = self.app.mesh.texture[default_texture]
         self.hover_texture = self.app.mesh.texture[hover_texture]
+
+        self.change_delay_time = change_delay_time
+        self.last_change_time = time.time()
+        self.press_delay_time = sequent_press_delay_time
+
         self.is_dynamic = is_dynamic
-        self.last_switch_time = time.time()
+
+        self.is_chosen = False
+        self.func_on_click = lambda: None
+        self.button_up = [None, False]
+        self.button_down = [None, False]
+        self.button_left = [None, False]
+        self.button_right = [None, False]
+
         self.on_init()
 
     def on_init(self):
         # texture
         self.program["u_texture_0"] = 0
-        self.use_correct_texture()
+        self.set_correct_texture()
         self.texture.use(location=0)
         # mvp
         self.program["m_proj"].write(self.app.camera.m_proj)
         self.program["m_view"].write(self.app.camera.m_view)
         self.program["m_model"].write(self.m_model)
 
-    def use_correct_texture(self):
+    def set_correct_texture(self):
         self.texture = self.hover_texture if self.is_chosen else self.default_texture
 
-    def up_button(self, button):
-        self.button_up = button
-        self.button_up_taken = True
-
-    def down_button(self, button):
-        self.button_down = button
-        self.button_down_taken = True
-
-    def left_button(self, button):
-        self.button_left = button
-        self.button_left_taken = True
-
-    def right_button(self, button):
-        self.button_right = button
-        self.button_right_taken = True
-
-    def is_clicked(self):
-        current_time = time.time()
-        if current_time - self.last_click_time < self.press_delay_time:
-            return False
-
-        if self.is_chosen:
-            keys = pg.key.get_pressed()
-            if keys[self.key_binds.button_press[0]] or keys[self.key_binds.button_press[1]]:
-                Button.last_click_time = current_time
-                return True
-
-        return False
-
     def set_chosen(self, button_and_id=None):
-        """
-        button_and_id: of the form (button, id)
-        id: of the form ("up", "down", "left", "right")
-        """
         self.is_chosen = True
 
         if not (self.is_dynamic and button_and_id):
@@ -89,54 +55,80 @@ class Button(BaseModel):  # TODO: Refactor this class
         button = button_and_id[0]
         match button_and_id[1]:
             case "up":
-                if not (self.button_down and self.button_down_taken):
-                    self.button_down = button
+                if not (self.button_down[0] and self.button_down[1]):
+                    self.button_down[0] = button
 
             case "down":
-                if not (self.button_up and self.button_up_taken):
-                    self.button_up = button
+                if not (self.button_up[0] and self.button_up[1]):
+                    self.button_up[0] = button
 
             case "left":
-                if not (self.button_right and self.button_right_taken):
-                    self.button_right = button
+                if not (self.button_right[0] and self.button_right[1]):
+                    self.button_right[0] = button
 
             case "right":
-                if not (self.button_left and self.button_left_taken):
-                    self.button_left = button
+                if not (self.button_left[0] and self.button_left[1]):
+                    self.button_left[0] = button
 
     def set_not_chosen(self, current_time=None):
         self.is_chosen = False
         if current_time is not None:
-            self.last_switch_time = current_time
-
-    def listen_for_change(self):
-        current_time = time.time()
-        if current_time - self.last_switch_time < self.change_delay_time:
-            return
-
-        keys = pg.key.get_pressed()
-        if keys[self.key_binds.button_up[0]] or keys[self.key_binds.button_up[1]]:
-            if self.button_up:
-                self.set_not_chosen(current_time)
-                self.button_up.set_chosen(button_and_id=(self, "up"))
-
-        elif keys[self.key_binds.button_down[0]] or keys[self.key_binds.button_down[1]]:
-            if self.button_down:
-                self.set_not_chosen(current_time)
-                self.button_down.set_chosen(button_and_id=(self, "down"))
-
-        elif keys[self.key_binds.button_left[0]] or keys[self.key_binds.button_left[1]]:
-            if self.button_left:
-                self.set_not_chosen(current_time)
-                self.button_left.set_chosen(button_and_id=(self, "left"))
-
-        elif keys[self.key_binds.button_right[0]] or keys[self.key_binds.button_right[1]]:
-            if self.button_right:
-                self.set_not_chosen(current_time)
-                self.button_right.set_chosen(button_and_id=(self, "right"))
+            self.last_change_time = current_time
 
     def on_click(self, func):
         self.func_on_click = func
+
+    def up_button(self, button):
+        self.button_up = [button, True]
+
+    def down_button(self, button):
+        self.button_down = [button, True]
+
+    def left_button(self, button):
+        self.button_left = [button, True]
+
+    def right_button(self, button):
+        self.button_right = [button, True]
+
+    def is_clicked(self):
+        current_time = time.time()
+        if current_time - self.last_press_time < self.press_delay_time:
+            return False
+
+        if self.is_chosen:
+            keys = pg.key.get_pressed()
+            if keys[self.app.key_binds.button_press[0]] or keys[self.app.key_binds.button_press[1]]:
+                Button.last_press_time = current_time
+                return True
+
+        return False
+
+    def listen_for_change(self):
+        current_time = time.time()
+        if current_time - self.last_change_time < self.change_delay_time:
+            return
+
+        key_binds = self.app.key_binds
+        keys = pg.key.get_pressed()
+        if keys[key_binds.button_up[0]] or keys[key_binds.button_up[1]]:
+            if self.button_up[0]:
+                self.set_not_chosen(current_time)
+                self.button_up[0].set_chosen(button_and_id=(self, "up"))
+
+        elif keys[key_binds.button_down[0]] or keys[key_binds.button_down[1]]:
+            if self.button_down[0]:
+                self.set_not_chosen(current_time)
+                self.button_down[0].set_chosen(button_and_id=(self, "down"))
+
+        elif keys[key_binds.button_left[0]] or keys[key_binds.button_left[1]]:
+            if self.button_left[0]:
+                self.set_not_chosen(current_time)
+                self.button_left[0].set_chosen(button_and_id=(self, "left"))
+
+        elif keys[key_binds.button_right[0]] or keys[key_binds.button_right[1]]:
+            if self.button_right[0]:
+                self.set_not_chosen(current_time)
+                self.button_right[0].set_chosen(button_and_id=(self, "right"))
 
     # @override
     def update(self):
@@ -145,12 +137,12 @@ class Button(BaseModel):  # TODO: Refactor this class
             self.listen_for_change()
 
         else:
-            self.last_switch_time = time.time()
+            self.last_change_time = time.time()
 
         if self.is_clicked():
             self.func_on_click()
 
-        self.use_correct_texture()
+        self.set_correct_texture()
         self.texture.use(location=0)
         self.program["m_model"].write(self.m_model)
         self.program["m_view"].write(self.app.camera.m_view)
