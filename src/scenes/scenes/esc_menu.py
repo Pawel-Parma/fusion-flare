ee33572d0102948211cc00762a72d6d6c66145ae
Pawel-Parma <122152230+Pawel-Parma@users.noreply.github.com>
2024-02-22 21:50:44 +0100

    feat: updated menu scenes

--- src/scenes/scenes/esc_menu.py
+++ src/scenes/scenes/esc_menu.py
@@ -17,12 +17,15 @@ class EscMenuScene(BaseScene):
         # buttons
         hover_texture = "white"
         # play
-        play_button = add(Button(app, (0, -0.5, 0), "bronze_panel", hover_texture, scale=(1.5, 0.5)))
+        play_button = add(Button(app, (0, -0.5, 0), "white", hover_texture, scale=(1.5, 0.5),
+                                 default_color=(0, 125, 0), hover_color=(0, 255, 0)))
         play_button.on_click(app.play)
         # settings
-        settings_button = add(Button(app, (0, -2, 0), "black", hover_texture, scale=(1.5, 0.5)))
+        settings_button = add(Button(app, (0, -2, 0), "black", hover_texture, scale=(1.5, 0.5),
+                                     hover_color=(255, 255, 0)))
         # exit
-        exit_button = add(Button(app, (0, -3.5, 0), "wooden_box", hover_texture, scale=(1.5, 0.5)))
+        exit_button = add(Button(app, (0, -3.5, 0), "white", hover_texture, scale=(1.5, 0.5),
+                                 default_color=(125, 0, 0), hover_color=(255, 0, 0)))
         exit_button.on_click(app.main_menu)
         # bind buttons
         play_button.set_chosen()
