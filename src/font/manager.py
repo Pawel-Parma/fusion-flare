dc55a41742368fe9a6ef470c8451651d00f85274
Pawel-Parma <122152230+Pawel-Parma@users.noreply.github.com>
2024-05-06 22:09:52 +0200

    chore: changed some names

--- src/font/manager.py
+++ src/font/manager.py
@@ -7,14 +7,14 @@ from .font import Font
 class FontManager:
     def __init__(self, app):
         self.app = app
-        self.font_list = set(op.splitext(font)[0] for font in os.listdir(app.fonts_dir_path))
-        self.fonts = {}
+        self.available_fonts = set(op.splitext(font)[0] for font in os.listdir(app.fonts_dir_path))
+        self.loaded_fonts = {}
 
     def __getitem__(self, full_name: tuple[str, tuple[int, int]]):
-        if full_name[0] not in self.font_list:
+        if full_name[0] not in self.available_fonts:
             raise Exception(f"Font ({full_name[0]}) not found")
 
-        if full_name not in self.fonts:
-            self.fonts[full_name] = Font(self.app, *full_name)
+        if full_name not in self.loaded_fonts:
+            self.loaded_fonts[full_name] = Font(self.app, *full_name)
 
-        return self.fonts[full_name]
+        return self.loaded_fonts[full_name]
